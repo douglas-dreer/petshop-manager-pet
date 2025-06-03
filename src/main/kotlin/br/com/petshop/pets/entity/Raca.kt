82a1b22b9d8033c65bc074694872c709370b2bc6
Douglas <github.barzeus@outlook.com.br>
2025-06-03 16:14:26 -0300

    add: create Raca entity with Hibernate annotations

---
+++ src/main/kotlin/br/com/petshop/pets/entity/Raca.kt
@@ -0,0 +1,29 @@
+package br.com.petshop.pets.entity
+
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "TBL_0001_RACAS", schema = "gerenciamento_pets")
+class Raca(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    @Column(name = "nome", nullable = false, unique = true)
+    val nome: String,
+
+    @ManyToOne
+    @JoinColumn(name = "especie_id", nullable = false)
+    val especie: Especie,
+
+    @CreationTimestamp
+    @Column(name = "data_criacao", updatable = false)
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+
+    @UpdateTimestamp
+    @Column(name = "data_modificacao")
+    val dataModificacao: LocalDateTime? = null,
+) {
+}
